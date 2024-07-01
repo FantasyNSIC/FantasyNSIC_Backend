07ca7c9f0c4252547be495a2cd0f2224f081364f
JThoe26 <jackson.thoe@mnsu.edu>
2024-07-01 09:09:23 -0500

    include weeks 2 and 3

--- src/queries/get_nsic_player_stats_weekly.sql
+++ src/queries/get_nsic_player_stats_weekly.sql
@@ -1,4 +1,6 @@
-SELECT stW1.*, points.week_1
+SELECT stW1.*, points.week_1, stW2.*, points.week_2, stW3.*, points.week_3
 FROM player_stats_week_1 stW1
 JOIN player_points_scored points ON points.player_id = stW1.player_id
+JOIN player_stats_week_2 stW2 ON stW2.player_id = stW1.player_id
+JOIN player_stats_week_3 stW3 ON stW3.player_id = stW1.player_id
 WHERE stW1.player_id = %s
